57cdf0d618d4476631e1875dba0ca13de0732cd1
AMISEF <46823959+AMISEF@users.noreply.github.com>
2025-05-03 22:13:33 +0330

    Update Multiplication table-HW1.m

--- Multiplication table-HW1.m
+++ Multiplication table-HW1.m
@@ -1,3 +1,4 @@
+% Multiplication table
 >> table = (1 : 10)' * (1 : 10);
 >> disp (table);
      1     2     3     4     5     6     7     8     9    10
@@ -12,3 +13,4 @@
     10    20    30    40    50    60    70    80    90   100
 
 >> 
+% AMISEF STUDIO
